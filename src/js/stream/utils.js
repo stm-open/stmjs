15580c26e296ddabee7eeda4cd0e13bb8c59d88f
stm-open <74229221+stm-open@users.noreply.github.com>
2021-04-22 15:56:37 +0800

    fix
    sjcl require fix

--- src/js/stream/utils.js
+++ src/js/stream/utils.js
@@ -152,7 +152,7 @@ exports.fromTimestamp = fromTimestamp;
 
 // Going up three levels is needed to escape the src-cov folder used for the
 // test coverage stuff.
-exports.sjcl = require('../../../build/sjcl');
+exports.sjcl = require('../../../src/js/sjcl/sjcl');
 exports.jsbn = require('../../../src/js/jsbn/jsbn');
 
 // vim:sw=2:sts=2:ts=8:et
